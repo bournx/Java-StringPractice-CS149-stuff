abe47c278b96bd1fa79e502d73318231b1f45745
Nico <70189104+bournx@users.noreply.github.com>
2020-12-14 12:27:34 -0500

    Add files via upload

---
+++ FamousQuotes.java
@@ -0,0 +1,48 @@
+/**
+ * James Madison Quotes.
+ *
+ * @author Nico Bour
+ */
+public class FamousQuotes {
+   
+    public static void main(String[] args) {
+      
+        System.out.print("Top 10 James Madison Quotes ");
+        System.out.println("(according to brainyquote.com)");
+        System.out.print("-----------------------------");
+        System.out.println("-----------------------------");
+        System.out.print("[10] The essence of Government is power; ");
+        System.out.print("and power, lodged as it must be "); 
+        System.out.println("in human hands, will ever be liable to abuse.");
+        System.out.print("[9] The truth is that all men having power ought ");
+        System.out.println("to be mistrusted.");
+        System.out.print("[8] The advancement and diffusion of knowledge ");
+        System.out.println("is the only guardian of true liberty.");
+        System.out.print("[7] What is government itself but the greatest ");
+        System.out.println("of all reflections on human nature?");
+        System.out.print("[6] The people are the only legitimate fountain "); 
+        System.out.print("of power, and it is from them that the ");
+        System.out.print("constitutional charter, under which the several ");
+        System.out.print("branches of government hold their power, "); 
+        System.out.println("is derived.");
+        System.out.print("[5] America was indebted to immigration for her ");
+        System.out.print("settlement and prosperity. That part of America ");
+        System.out.print("which had encouraged them most had advanced most ");  
+        System.out.println("rapidly in population, agriculture and the arts.");
+        System.out.print("[4] Knowledge will forever govern ignorance; and a ");
+        System.out.print("people who mean to be their own governors must arm ");
+        System.out.println("themselves with the power which knowledge gives.");
+        System.out.print("[3] Liberty may be endangered by the abuse ");
+        System.out.println("of liberty, but also by the abuse of power.");
+        System.out.print("[2] The happy Union of these States is a wonder; ");
+        System.out.print("their Constitution a miracle; their example ");
+        System.out.println("the hope of Liberty throughout the world.");
+        System.out.print("[1] If men were angels, no government ");
+        System.out.println("would be necessary.");
+
+
+
+
+        
+    }
+}
---
+++ FightSong.java
@@ -0,0 +1,24 @@
+/**
+ * Fight Song
+ *
+ * @author Nico Bour
+ */
+public class FightSong {
+
+   public static void main(String[] args) {
+   
+      System.out.println("Madison, James Madison,");
+      System.out.println("We are the Dukes of JMU");
+      System.out.println("Madison, James Madison,");
+      System.out.println("The fighting Dukes of JMU");
+      System.out.println("Fight for Glory-- Honors Won");
+      System.out.println("Brighten the Lights of Madison");
+      System.out.println("Madison, James Madison,");
+      System.out.println("Show your Colors");
+      System.out.println("Proud and True");
+      System.out.println("We are the Dukes of JMU");
+   
+    
+   }
+}
+ 
